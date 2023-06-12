79b4b1de8bdf8a3695809d77a386507ed55fb2d6
Tim Jacomb <timjacomb1@gmail.com>
2023-06-12 15:00:53 +0100

    This

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     testTimeout: 60 * 1000,
+    hookTimeout: 60 * 1000,
     coverage: {
       provider: "v8",
       exclude: [".pnp.cjs", ".pnp.loader.mjs"],
