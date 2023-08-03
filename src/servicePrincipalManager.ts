e2d82fd4661f9832073cf69de6e703d5f944e412
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2023-08-03 14:02:00 +0100

    Update graphql perms if existing (#33)

--- src/servicePrincipalManager.ts
+++ src/servicePrincipalManager.ts
@@ -351,22 +351,42 @@ export async function grantOauth2Permissions({
 
     const graphAPIObjectId = (await graphAPIIDResult.json()).value[0].id;
 
-    const grantPermissionsResult = await fetch(
-      `https://graph.microsoft.com/v1.0/oauth2PermissionGrants`,
+    const searchGrantsResponse = await fetch(
+      `https://graph.microsoft.com/v1.0/oauth2PermissionGrants?$filter=(clientId eq '${objectId}' and resourceId eq '${graphAPIObjectId}')`,
       {
-        method: "POST",
+        method: "Get",
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          clientId: `${objectId}`,
-          consentType: "AllPrincipals",
-          resourceId: `${graphAPIObjectId}`,
-          scope: `${oauth2Permissions.join(" ")}`,
-        }),
       },
     );
+    await errorHandler("Granting aouth2 permissions", searchGrantsResponse);
+    const grants = await searchGrantsResponse.json();
+
+    console.log(grants);
+    let grantPermissionUrl =
+      "https://graph.microsoft.com/v1.0/oauth2PermissionGrants";
+    let grantPermissionMethod = "POST";
+
+    if (grants && grants.value.length > 0) {
+      grantPermissionMethod = "PATCH";
+      grantPermissionUrl = `https://graph.microsoft.com/v1.0/oauth2PermissionGrants/${grants.value[0].id}`;
+    }
+
+    const grantPermissionsResult = await fetch(`${grantPermissionUrl}`, {
+      method: `${grantPermissionMethod}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        clientId: `${objectId}`,
+        consentType: "AllPrincipals",
+        resourceId: `${graphAPIObjectId}`,
+        scope: `${oauth2Permissions.join(" ")}`,
+      }),
+    });
     await errorHandler("Granting aouth2 permissions", grantPermissionsResult);
   }
 }
